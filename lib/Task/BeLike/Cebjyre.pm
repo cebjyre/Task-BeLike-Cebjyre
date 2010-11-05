7fb79105a9d6a37126783c8589def5ca772dc306
Glenn Fowler <cebjyre@cpan.org>
2010-11-05 15:25:29 +1100

    Initial commit
    Includes a few modules

---
+++ lib/Task/BeLike/Cebjyre.pm
@@ -0,0 +1,45 @@
+use strict;
+use warnings;
+package Task::BeLike::Cebjyre;
+
+# ABSTRACT: Modules I find handy from time to time...
+
+=pkgroup Email stuff
+
+=pkg Email::Sender
+
+=pkg MIME::Parser
+
+Also gives us Moose
+
+=pkgroup Data Access
+
+=pkg Cache::Memcached::Fast
+
+=pkg Net::LDAP
+
+=pkgroup The Interwebs
+
+=pkg CGI::Session
+
+=pkgroup Misc
+
+=pkg File::chdir
+
+=pkg Log::Log4perl
+
+=pkg Module::Find
+
+=pkg Telephony::CountryDialingCodes
+
+=pkg Template
+
+=pkg Text::Context
+
+=pkg Tie::SecureHash
+
+=pkg XML::Quote
+
+=cut
+
+1;
